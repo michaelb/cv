e2d348ec6fcef5ec1d7ff5914c69eba7d741ee69
Michael Bleuez <michael.bleuez2@gmail.com>
2021-06-12 22:20:49 +0200

    french localization

--- src/build.js
+++ src/build.js
@@ -2,6 +2,7 @@ const handlebars = require('handlebars');
 const fs = require('fs-extra');
 const markdownHelper = require('./utils/helpers/markdown');
 const templateData = require('./metadata/metadata');
+const templateDataFr = require('./metadata/metadatafr');
 const Puppeteer = require('puppeteer');
 const getSlug = require('speakingurl');
 const dayjs = require('dayjs');
@@ -10,25 +11,40 @@ const username = require('git-username');
 
 const srcDir = __dirname;
 const outputDir = __dirname + '/../dist';
+const outputDirFr = __dirname + '/../dist/fr';
 
 // Clear dist dir
 fs.emptyDirSync(outputDir);
+fs.emptyDirSync(outputDirFr);
 
 // Copy assets
 fs.copySync(srcDir + '/assets', outputDir);
+// fs.copySync(srcDir + '/assets/fr', outputDirFr);
 
 // Build HTML
 handlebars.registerHelper('markdown', markdownHelper);
 const source = fs.readFileSync(srcDir + '/templates/index.html', 'utf-8');
+const sourcefr = fs.readFileSync(srcDir + '/templates/indexfr.html', 'utf-8');
 const template = handlebars.compile(source);
+const templatefr = handlebars.compile(sourcefr);
 const pdfFileName = `${getSlug(templateData.name)}.${getSlug(templateData.title)}.pdf`;
+const pdfFileNameFr = `/fr/${getSlug(templateDataFr.name)}.${getSlug(templateDataFr.title)}.pdf`;
 const html = template({
   ...templateData,
   baseUrl: `https://${username()}.github.io/${repoName.sync()}`,
   pdfFileName,
   updated: dayjs().format('MMMM D, YYYY'),
 });
+
+const htmlfr = templatefr({
+  ...templateDataFr,
+  baseUrl: `https://${username()}.github.io/${repoName.sync()}/fr`,
+  pdfFileNameFr,
+  updated: dayjs().format('D/MM/YYYY'),
+});
+
 fs.writeFileSync(outputDir + '/index.html', html);
+fs.writeFileSync(outputDir + '/fr/index.html', htmlfr);
 
 buildPdf = async function (inputFile, outputFile) {
   const browser = await Puppeteer.launch();
@@ -52,3 +68,4 @@ buildPdf = async function (inputFile, outputFile) {
 
 // Build PDF
 buildPdf(`${outputDir}/index.html`, `${outputDir}/${pdfFileName}`);
+buildPdf(`${outputDir}/fr/index.html`, `${outputDir}/${pdfFileNameFr}`);
--- src/metadata/metadatafr.js
+++ src/metadata/metadatafr.js
@@ -1,9 +1,9 @@
 module.exports = {
   name: 'Michael Bleuez',
-  title: 'Software Engineer',
-  baseUrl: 'https://michaelb.github.io/cv/',
+  title: 'Ingénieur en Informatique',
+  baseUrl: 'https://michaelb.github.io/cv/fr',
   facts: {
-    "Residence": '<a href="https://www.google.com/maps/place/Grenoble/@45.1842207,5.6804372,13z/data=!4m5!3m4!1s0x478af48bd689be6f:0x618c10cd6e995398!8m2!3d45.188529!4d5.724524" target="_blank"><i class="fa fa-home"></i>Grenoble, France</a>',
+    "Résidence": '<a href="https://www.google.com/maps/place/Grenoble/@45.1842207,5.6804372,13z/data=!4m5!3m4!1s0x478af48bd689be6f:0x618c10cd6e995398!8m2!3d45.188529!4d5.724524" target="_blank"><i class="fa fa-home"></i>Grenoble, France</a>',
     "   LinkedIn": ' <a href="https://www.linkedin.com/in/michael-bleuez-b2b737190/" target="_blank"><i class="fa fa-linkedin"></i>michael bleuez</a>',
     "     GitHub": '  <a href="https://github.com/michaelb" target="_blank"><i class="fa fa-github"></i>michaelb</a>',
     "       Email": '  <a href="mailto:michael.bleuez2@gmail.com" target="_blank"><i class="fa fa-envelope"></i>michael.bleuez2@gmail.com</a>',
@@ -47,77 +47,47 @@ module.exports = {
   },
   experience: [
     {
-      title: 'End of studies internship',
+      title: "Stage de fin d'études",
       place: 'Atos R&D Data Management',
       link: 'https://atos.net/en/',
-      date: 'Feb-Aug 2021',
+      date: 'Février-Août 2021',
       badges: ['C', 'HPC', 'IO'],
       contents: `
 <ul>
-<li> Study the state-of-art of caching technology.</li>
-<li> Identify performance bottlenecks of an <a href="https://atos.net/en/2019/product-news_2019_02_07/atos-boosts-hpc-application-efficiency-new-flash-accelerator-solution" style="text-decoration:underline !important;color:inherit;">IO accelerator</a> used on HPC systems, and develop solutions to improve the global throughput of the product.</li>
-<li>Work with an Agile team.</li>
+<li> Identification des problèmes de performance d'un <a href="https://atos.net/en/2019/product-news_2019_02_07/atos-boosts-hpc-application-efficiency-new-flash-accelerator-solution" style="text-decoration:underline !important;color:inherit;">accélérateur IO</a> utilisé par des environements HPC, et développement d'une solution qui résoud lesdits problèmes.</li>
+<li>Travail au sein d'une équipe Agile.</li>
 </ul>
 `
     },
     {
-
-      title: 'Part-time job',
-      place:'IT support dept. Ensimag',
+      title: 'Support Informatique (temps partiel)',
+      place:'Ensimag Grenoble-INP, Service Informatique',
       link: 'https://ensimag.grenoble-inp.fr/',
       date: '2018-2021',
       badges: ['Linux', 'IT support'],
       contents: `
 <ul>
-<li>Provide day-to-day support to students and help them install and manage a Linux-based work environment.</li>
-<li>Take initiatives to improve quality-of-life services such as documentation, installers and scripts.</li>
-<li>Work with the IT support staff to design future services, perform maintenance and upgrade hardware.</li>
+<li>Support informatique aux étudiants, installer et gérer un environment de travail sous Linux</li>
+<li>Prise d'initiative pour améliorer les services, documentation et scripts fournis aux élèves</li>
 </ul>
 `
     },
-    {
-      title: 'Summer internship',
-      place: 'RHEA Group (Redu, Belgium)',
-      date: 'Jul-Aug 2019',
-      link: 'https://www.rheagroup.com/',
-      badges: ['virtualization', 'cybersecurity'],
-      contents: `
-<ul>
-<li> Complete different, independent parts of a proof-of-concept cybersecurity project (<a href="https://github.com/michaelb/go-passwd-crack" style="text-decoration:underline !important;color:inherit;" >password cracking</a>, <a href="https://github.com/michaelb/simple-user-simulation" style="text-decoration:underline !important;color:inherit;">user simulation</a>, SCADA/PLC system) in coordination with an english-speaking, international team.</li>
-<li> Create reproducible VM and software setups.</li>
-</ul>
-`
-    }
   ],
   projects: [
     {
       header: '<a href="https://github.com/michaelb/sniprun" style="text-decoration:none !important; color:inherit;" target="_blank">Sniprun</a>',
-      skills: ['Rust', 'Lua', 'Neovim plugin'],
+      skills: ['Rust', 'Lua', 'DevOps'],
       link: 'https://github.com/michaelb/sniprun',
       contents: `
-<a href="https://github.com/michaelb/sniprun" style="text-decoration:underline !important; color:inherit;" target="_blank">Sniprun</a> is my flagship open-source project. It allows users to run snippets of code directly from the editor (Neovim). In addition to the code itself, sniprun has a CI pipeline, a 'deployment' system, tests and coverage, extensive documentation, and showcases team & community work. 
+<a href="https://github.com/michaelb/sniprun" style="text-decoration:underline !important; color:inherit;" target="_blank">Sniprun</a>, un plugin pour neovim, est mon plus 'grand' projet open-source. En plus de la fonctionnalité intéressante en elle-même (exécuter du code depuis l'éditeur), sniprun possède une pipeline d'intégration continue assez avancée et témoigne d'aspects de gestion de projet & communauté.
 `
     },
-    {
-      header: 'Compiler',
-      skills: ['Java', 'DevOps'],
-      contents: `
-A one-month long team assignment to create a compiler written in Java, used on an arbitrary object-oriented langage to generate machine code and LLVM IR.
-`
-    },
-    {
-      header: '<a href="https://github.com/michaelb?tab=repositories&q=&type=source&language=&sort=stargazers" target="_blank" style="text-decoration:none !important; color:inherit;">... and many others:</a>',
+  {
+      header: '<a href="https://github.com/michaelb?tab=repositories&q=&type=source&language=&sort=stargazers" target="_blank" style="text-decoration:none !important; color:inherit;">... et bien d\'autres que vous pouvez trouver sur mon profil:</a>',
       skills:[],
       contents:`
-<ul>
-<li> <a href="https://github.com/michaelb/shogai" target="_blank" style="text-decoration:underline !important; color:inherit;">shogai</a> and <a href="https://github.com/michaelb/shogui" target="_blank" style="text-decoration:underline !important; color:inherit;">shogui</a>, a shogi library and GUI both written in Rust </li>
-<li> a surface reconstruction algorithm, in Python </li>
-<li> hash crackers (<a href="https://github.com/michaelb/go-passwd-crack" target="_blank" style="text-decoration:underline !important; color:inherit;">one</a>  in PyQt/Go, another in C++/OpenCL/GPU) </li>
-<li> a  <a href="https://github.com/michaelb/auto-ytdl" target="_blank" style="text-decoration:underline !important; color:inherit;">youtube-dl</a> wrapper for music, in Python </li>
-<li> a generic   <a href="https://github.com/michaelb/point-clustering" target="_blank" style="text-decoration:underline !important; color:inherit;">point clustering algorithm</a> in Python </li>
-<li> a  <a href="https://github.com/michaelb/simple_prime_generator" target="_blank" style="text-decoration:underline !important; color:inherit;">prime numbers generator</a>, in C </li>
-</ul>
-`
+Librairie et interface graphique pour du shogi (<a href="https://github.com/michaelb/shogai" target="_blank" style="text-decoration:underline !important; color:inherit;">shogai</a> and <a href="https://github.com/michaelb/shogui" target="_blank" style="text-decoration:underline !important; color:inherit;">shogui</a>), un compilateur, un algorithme de reconstruction de surfaces, <a href="https://github.com/michaelb/go-passwd-crack" target="_blank" style="text-decoration:underline !important; color:inherit;"> un hash-cracker</a>...`
     },
-  ]
+
+   ]
 };
